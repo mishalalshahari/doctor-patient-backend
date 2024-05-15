794e3423562068847dd6c7afa60f5891e5dddb15
mishalalshahari <mishalalshahari@gmail.com>
2024-05-16 00:33:09 +0530

    jpa fix

--- src/main/java/org/example/doctorpatientbackend/repository/DoctorRepo.java
+++ src/main/java/org/example/doctorpatientbackend/repository/DoctorRepo.java
@@ -1,10 +1,11 @@
 package org.example.doctorpatientbackend.repository;
 
-import org.example.doctorpatientbackend.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import org.example.doctorpatientbackend.entities.Doctor;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,6 @@ public interface DoctorRepo extends JpaRepository<Doctor, Integer> {
 
     Optional<Doctor> findByEmailAndPassword(String email, String password);
 
-    @Query(value = "select * from doctor d where d.city = :city", nativeQuery = true)
+    @Query(value = "select * from Doctor d where d.city = :city", nativeQuery = true)
     List<Doctor> getDoctorBasedOnLocation(@Param("city") String city);
 }
